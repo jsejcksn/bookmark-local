ebe777c367053e485d7b4a250cc2745cbe4d3946
Jesse Jackson <jsejcksn@users.noreply.github.com>
2016-11-11 00:13:04 -0600

    Add multi-page functionality 😅

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
-(() => {
+const pagemark = (() => {
   'use strict';
 
-  let pagemark;
+  let pagemark = {
+    pages: []
+  };
   init();
+  getPagemark();
 
-  // Scroll to saved position
+  // Scroll to saved position if possible
   function getPagemark () {
-    window.scrollTo(0, pagemark);
+    for (let i = 0; i < pagemark.pages.length; i++) {
+      if (pagemark.pages[i].url === window.location.href) {
+        window.scrollTo(0, pagemark.pages[i].mark);
+        console.log('Got mark; you\'re back.');
+        break;
+      }
+    }
   }
 
-  // Add event listener; if possible, scroll to saved position
+  // Retrieve pagemark from localStorage; add event listener
   function init () {
-    window.addEventListener('scroll', setPagemark);
-    pagemark = localStorage.getItem('pagemark');
-    if (pagemark !== null && typeof pagemark !== 'undefined' && pagemark !== '') { // localStorage key exists and not empty
-      getPagemark();
-    } else {
-      return;
+    const storage = localStorage.getItem('pagemark');
+    if (storage !== null && typeof storage !== 'undefined' && storage !== '') { // localStorage key exists and not empty
+      pagemark = JSON.parse(storage);
     }
+    window.addEventListener('scroll', setPagemark);
   }
 
   // Save scrolled posiiton
   function setPagemark () {
-    localStorage.setItem('pagemark', document.body.scrollTop);
+    const newMark = {
+      url: window.location.href,
+      mark: document.body.scrollTop
+    };
+    if (pagemark.pages.length === 0) {
+      pagemark.pages.push(newMark);
+    } else {
+      for (let i = 0; i < pagemark.pages.length; i++) {
+        if (pagemark.pages[i].url === window.location.href) {
+          pagemark.pages[i].mark = newMark.mark;
+          break;
+        } else if (i === pagemark.pages.length - 1) {
+          pagemark.pages.push(newMark);
+        }
+      }
+    }
+    localStorage.setItem('pagemark', JSON.stringify(pagemark));
   }
+
+  return {
+    get: getPagemark
+  };
 
 })();
